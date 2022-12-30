0971fcf00df17c9fbb3b327211d11508010dc20a
Sarp Cakir <sarpcakir17@gmail.com>
2022-12-30 16:13:26 +0300

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h1 className='text-4xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className='text-lg'>
+        Version: <span>1.0.0</span>
+      </p>
+    </div>
+  )
+}
+
+export default About
